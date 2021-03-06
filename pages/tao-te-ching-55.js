fe86c89ffe237a7019cbadef4788032a7a92b218
Graham Murdoch <murdoch@uber.com>
2021-03-05 20:44:03 -0800

    Add Tao Te Ching Chapter 55 (#2)

---
+++ pages/tao-te-ching-55.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tao Te Ching (55)</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Tao Te Ching (55)</h1>
+        <p>
+          He who is filled with Virtue is like a newborn child.<br />
+          Wasps and serpents will not sting him;<br />
+          Wild beasts will not pounce upon him;<br />
+          He will not be attacked by birds of prey.<br />
+          His bones are soft, his muscles weak,<br />
+          But his grip is firm.<br />
+          He has not experienced the union of man and woman, but is whole.<br />
+          His manhood is strong.<br />
+          He screams all day without becoming hoarse.<br />
+          This is perfect harmony.<br />
+        <p>
+        </p>
+          Knowing harmony is constancy.<br />
+          Knowing constancy is enlightenment.<br />
+        <p>
+        </p>
+          It is not wise to rush about.<br />
+          Controlling the breath causes strain.<br />
+          If too much energy is used, exhaustion follows.<br />
+          This is not the way of Tao.<br />
+          Whatever is contrary to Tao will not last long.<br />
+        </p>
+        <p>
+          Lao Tzu<br />
+          Translated by Gia-fu Feng & Jane English
+        </p>
+      </main>
+      <footer>
+        <a href="/">Home</a>
+      </footer>
+    </>
+  )
+}
