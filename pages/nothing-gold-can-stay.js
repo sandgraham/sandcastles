c1323b423943684de3ccfd1007f17118c8b98886
Graham Murdoch <grahammurdoch8@gmail.com>
2025-04-08 15:34:56 -0700

    Create nothing-gold-can-stay.js

---
+++ pages/nothing-gold-can-stay.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { PoemPage } from "../components/PoemPage";
+//
+export default Page;
+
+function Page() {
+  return (
+    <PoemPage title={"Nothing Gold Can Stay"} author={"Robert Frost"}>
+      <p>
+        Nature's first green is gold,
+        <br />
+        Her hardest hue to hold.
+        <br />
+        Her early leaf's a flower;
+        <br />
+        But only so an hour.
+        <br />
+        Then leaf subsides to leaf.
+        <br />
+        So Eden sank to grief,
+        <br />
+        So dawn goes down to day.
+        <br />
+        Nothing gold can stay.
+        <br />
+      </p>
+    </PoemPage>
+  );
+}
